75839b07b148b64300d8a89f79a7bf07208ca03b
Christian Arty <artychristian@gmail.com>
2019-12-18 13:40:42 -0500

    small change to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { ThemeProvider } from "emotion-theming"
 import theme from "./src/styles/theme"
-import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 
 export const wrapRootElement = ({ element }) => {
   return <ThemeProvider theme={theme}>{element}</ThemeProvider>
