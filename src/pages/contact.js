72b7935bece1efbe4bb60c141a6bc41019299ed0
Christian Arty <artychristian@gmail.com>
2019-12-27 20:44:26 -0500

    Some small changes before deployment

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,43 +1,36 @@
 import React, { useState } from "react"
+import ContactInput from "../components/ContactInput"
 import PageLayout from "../templates/PageLayout"
 const Contact = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
+
   return (
     <PageLayout>
       <h1>Contact Me!</h1>
-      <form method="post" action="#">
-        <div>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Message:
-            <textarea
-              type="text"
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
-          </label>
-        </div>
+      <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+        <input type="hidden" name="bot-field" />
+        <ContactInput
+          label={"Name"}
+          value={name}
+          type={"text"}
+          onChange={setName}
+        />
+        <ContactInput
+          label={"Email"}
+          value={email}
+          type={"email"}
+          onChange={setEmail}
+        />
+        <ContactInput
+          textarea
+          label={"Message"}
+          value={message}
+          type={"text"}
+          onChange={setMessage}
+        />
+        <input type="submit" value="Contact Me" />
       </form>
     </PageLayout>
   )
