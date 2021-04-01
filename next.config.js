6248f58650fcdd3f4b426be1c0c55f4337bb4f70
Christian Arty <40256027+christianarty@users.noreply.github.com>
2021-04-01 01:53:28 -0400

    more setup but better

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
-module.exports = {
+const optimizedImages = require("next-optimized-images");
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
   future: {
     webpack5: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        handleImages: ["png"],
+        optimizeImagesInDev: true,
+        /* config for next-optimized-images */
+      },
+    ],
+  ],
+  nextConfig,
+);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import PropTypes from "prop-types";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.node.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default MyApp;
