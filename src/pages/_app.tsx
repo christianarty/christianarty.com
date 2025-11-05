2be0b1be98c0d3875976b352cc7868dcf9cef3cf
Christian Arty <40256027+christianarty@users.noreply.github.com>
2025-11-05 08:23:59 -0500

    add speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />;
     </>
   );
